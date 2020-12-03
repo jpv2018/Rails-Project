07a529f74a8bca1b43fc3806f572437af9364f4f
jpv2018 <jason.viola@mavs.uta.edu>
2020-12-02 19:40:30 -0600

    adds create function to judges controller with params

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -1,2 +1,13 @@
 class JudgesController < ApplicationController
+
+    def create(judge_params)
+        Judge.create(judge_params)
+    end
+
+    private
+
+    def judge_params
+        params.require(:judge).permit(:email, :password, :password_confirmation)
+    end
+    
 end
