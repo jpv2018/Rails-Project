c779319b1a94936f70a45cfe725840f24f9ebf99
jpv2018 <jason.viola@mavs.uta.edu>
2020-12-03 20:28:25 -0600

    adds player registration page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,18 @@
 class PlayersController < ApplicationController
+    skip_before_action :authorized
+
+    def new
+        @player = Player.new
+    end
+
+    def create
+        @player = Player.create(student_params)
+        redirect_to players_path
+    end
+
+    def index
+        @players = Player.all
+    end
+    
+
 end
