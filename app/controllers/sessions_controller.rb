8d99e727f9171f08f42286245640878c776aa316
jpv2018 <jason.viola@mavs.uta.edu>
2020-12-03 19:07:59 -0600

    adds login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,13 @@ class SessionsController < ApplicationController
 
     def create  
         @judge = Judge.find_by(email: params[:email])
-        return head(:forbidden) unless @judge.authenticate(params[:password])
-        session[:judge_id] = @judge.id
+        if @judge && @judge.authenticate(params[:password])
+            sessions[:judge_id] = @judge.id
+            redirect_to '/welcome'
+        else
+            redirect_to '/login'
+        end
+        
     end
 
 end
