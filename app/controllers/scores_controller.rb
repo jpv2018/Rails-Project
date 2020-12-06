3b1a21c48c63b6fc3f62ea10add449d12199f9d4
jpv2018 <jason.viola@mavs.uta.edu>
2020-12-06 15:33:25 -0600

    creates delete function for scores

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -30,6 +30,14 @@ class ScoresController < ApplicationController
         @score = Score.find_by(judge_id: params[:judge_id], player_id: params[:player_id])
     end
 
+    def destroy
+        @judge = current_user
+        @score = Score.find(params[:id])
+        @score.destroy
+        redirect_to judge_players_path(@judge)
+    end
+
+
     private
 
     def score_params
